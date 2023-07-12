3b76038b33ee3fcbe56754274917bd5d606dbb32
canilkumar <channamsettianilkumar@gmail.com>
2023-07-12 15:17:54 +0530

    first

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import boto3
+
+app = Flask(__name__)
+dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')  # Replace with your desired region
+table_name = 'Demotable'  # Replace with your DynamoDB table name
+table = dynamodb.Table(table_name)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    age = int(request.form.get('age'))
+    print(name+" " +str(age));
+    table.put_item(Item={'name': name, 'age': age})
+    return 'Data stored successfully in DynamoDB'
+
+@app.route('/usercount', methods=['GET', 'POST'])
+def usercount():
+    if request.method == 'POST':
+        password = request.form.get('password')
+        if password == 'Anil@0153':  # Replace 'your_password' with your actual password
+            response = table.scan()
+            count = response['Count']
+            return f'Total users: {count}'
+        else:
+            return 'Invalid password'
+    return render_template('usercount.html')
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(port=8080)
+    print(table_name)
+
+
